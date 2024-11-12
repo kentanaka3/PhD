e5ddbe1ef4128e93eec508fd474c17fc4c9c9175
Ken Tanaka <kentanaka3@outlook.com>
2024-11-12 17:21:55 +0100

    Removed Unnecessary files

--- src/Associator.py
+++ src/Associator.py
@@ -9,13 +9,15 @@ DATA_PATH = os.path.join(PRJ_PATH, "data")
 import sys
 # Add to path
 if INC_PATH not in sys.path: sys.path.append(INC_PATH)
+import json
 import obspy
 import argparse
 import scipy as sp
 import numpy as np
 import pandas as pd
+from pyproj import Proj
 import matplotlib.pyplot as plt
-from gamma.utils import association
+from gamma.utils import association, estimate_eps
 
 from constants import *
 import Picker as Pkr
@@ -39,14 +41,132 @@ def station_graph(inventory : obspy.Inventory) -> None:
   plt.savefig(IMG_FILE)
   return
 
-def associate_events(PRED : pd.DataFrame, STATION : pd.DataFrame,
-                     model_name : str, dataset_name : str, threshold : float) -> None:
+class AssociateConfig:
+  def __init__(self, INVENTORY : obspy.Inventory, center : tuple = None,
+                     x_lim : tuple = None, y_lim : tuple = None,
+                     z_lim : tuple = None, t_lim : tuple = None,
+                     vel : tuple = None, ncpu : int = 32,
+                     method : str = BAYES_GAUSS_MIX_MODEL_STR,
+                     use_amplitude : bool = False, file = None) -> None:
+    self.dims = [X_COORD_STR, Y_COORD_STR, Z_COORD_STR]
+    self.station = pd.DataFrame({(
+      PERIOD_STR.join([network.code, station.code]), station.longitude,
+      station.latitude, station.elevation * (-1e-3))
+      for network in INVENTORY for station in network},
+      columns=[ID_STR] + self.dims)
+    if file is not None:
+      # TODO: Implement the file reading method
+      pass
+    self.x_min = self.station[X_COORD_STR].min()
+    self.x_mid = self.station[X_COORD_STR].median()
+    self.x_max = self.station[X_COORD_STR].max()
+
+    self.y_min = self.station[Y_COORD_STR].min()
+    self.y_mid = self.station[Y_COORD_STR].median()
+    self.y_max = self.station[Y_COORD_STR].max()
+    print(self.x_min, self.x_mid, self.x_max)
+    print(self.y_min, self.y_mid, self.y_max)
+    self.center = (self.x_mid, self.y_mid)
+    if center: self.center = center
+    self.proj = Proj(f"+proj=sterea +lon_0={self.center[0]} "
+                     f"+lat_0={self.center[1]} +units=km")
+    # from deg to km
+    self.station[[X_COORD_STR, Y_COORD_STR]] = \
+      self.station.apply(lambda s:
+        pd.Series(self.proj(latitude=s[Y_COORD_STR],
+                            longitude=s[X_COORD_STR])), axis=1)
+
+    self.x_lim = (2 * self.x_min - self.x_mid, 2 * self.x_max - self.x_mid)
+    self.x_lim = self.proj(latitude=[self.center[1]] * 2,
+                           longitude=self.x_lim)[0]
+    if x_lim: self.x_lim = x_lim
+
+    self.y_lim = (2 * self.y_min - self.y_mid, 2 * self.y_max - self.y_mid)
+    self.y_lim = self.proj(latitude=self.y_lim,
+                           longitude=[self.center[0]] * 2)[1]
+    if y_lim: self.y_lim = y_lim
+
+    self.z_lim = (0, 30)
+    if z_lim: self.z_lim = z_lim
+    self.t_lim = (None, None)
+    if t_lim: self.t_lim = t_lim
+
+    self.bfgs_bounds = (
+      (self.x_lim[0] - 1, self.x_lim[1] + 1),
+      (self.y_lim[0] - 1, self.y_lim[1] + 1),
+      (self.z_lim[0], self.z_lim[1] + 1),
+      (self.t_lim[0], self.t_lim[1])
+    )
+
+    self.method = method
+    self.oversample_factor = 5 if method == BAYES_GAUSS_MIX_MODEL_STR else 1
+    self.use_amplitude = use_amplitude
+
+    self.vel = {
+      PWAVE: 5.85,
+      SWAVE: 5.85 / 1.78
+    }
+    if vel: self.vel = {key : value for key, value in zip([PWAVE, SWAVE], vel)}
+
+    # DBSCAN parameters
+    self.use_dbscan = False
+    self.dbscan_eps = None
+    self.dbscan_min_samples = 0
+    self.dbscan(3)
+
+    self.min_picks_per_eq = 5
+    self.min_p_picks_per_eq = 3
+    self.min_s_picks_per_eq = 2
+
+    self.max_sigma11 = 1.0
+    self.max_sigma22 = 1.0
+    self.max_sigma12 = 1.0
+
+    self.ncpu = ncpu
+    return
+
+  def __repr__(self) -> str:
+    return {
+      "dims": self.dims,
+      "use_dbscan": self.use_dbscan,
+      "use_amplitude": self.use_amplitude,
+      X_COORD_STR: self.x_lim,
+      Y_COORD_STR: self.y_lim,
+      Z_COORD_STR: self.z_lim,
+      "method": self.method,
+      "oversample_factor": self.oversample_factor,
+      "vel": {
+        "p" : self.vel[PWAVE],
+        "s" : self.vel[SWAVE]
+      },
+      "dbscan_eps": self.dbscan_eps,
+      "dbscan_min_samples": self.dbscan_min_samples,
+      "min_picks_per_eq": self.min_picks_per_eq,
+      "min_p_picks_per_eq": self.min_p_picks_per_eq,
+      "min_s_picks_per_eq": self.min_s_picks_per_eq,
+      "max_sigma11": self.max_sigma11,
+      "bfgs_bounds": self.bfgs_bounds
+    }
+
+  def __str__(self) -> str:
+    return f"AssociateConfig({self.__repr__()})"
+
+  def dbscan(self, min_samples = 3) -> None:
+    self.use_dbscan = True
+    self.dbscan_min_samples = min_samples
+    self.dbscan_eps = estimate_eps(self.station, self.vel[PWAVE])
+    return
+
+def associate_events(PRED : pd.DataFrame, config : AssociateConfig,
+                     model_name : str, dataset_name : str,
+                     threshold : float) -> None:
   if PRED.empty: return
   PRED[ID_STR] = PRED[NETWORK_STR] + PERIOD_STR + PRED[STATION_STR]
   PRED.rename(columns={PHASE_STR : TYPE_STR}, inplace=True)
   PRED[TIMESTAMP_STR] = PRED[TIMESTAMP_STR].apply(lambda x: x.datetime)
-  catalog, assignment = association(PRED, STATION, config=ASSOCIATION_CONFIG,
-                                    method=ASSOCIATION_CONFIG[METHOD_STR])
+  catalog, assignment = association(PRED, config.station,
+                                    config=config.__repr__(),
+                                    method=config.method)
   print(catalog)
   print(assignment)
   return
@@ -61,11 +181,8 @@ def main(args : argparse.Namespace) -> None:
   for station in stations:
     INVENTORY.extend(obspy.read_inventory(Path(DATA_PATH, STATION_STR,
                                                station + XML_EXT)))
-  HEADER = [ID_STR, X_COORD_STR, Y_COORD_STR, Z_COORD_STR]
-  STATION = {(PERIOD_STR.join([network.code, station.code]),
-              station.longitude, station.latitude, station.elevation)
-             for network in INVENTORY for station in network}
-  STATION = pd.DataFrame(STATION, columns=HEADER)
+  CONFIG = AssociateConfig(INVENTORY, file=args.file)
+  print(CONFIG)
   # if args.verbose: station_graph(INVENTORY)
   THRESHOLDS = [round(t, 2) for t in np.linspace(0.2, 0.9, 8)]
   PRED = Pkr.load_data(args)
@@ -73,7 +190,7 @@ def main(args : argparse.Namespace) -> None:
     for (model, dataset), dataframe in PRED.groupby([MODEL_STR, WEIGHT_STR]):
       dataframe = dataframe[dataframe[PROBABILITY_STR] >= threshold]\
                     .reset_index(drop=True)
-      associate_events(dataframe, STATION, model, dataset, threshold)
+      associate_events(dataframe, CONFIG, model, dataset, threshold)
   return
 
 if __name__ == "__main__": main(Pkr.parse_arguments())
---
+++ src/Stations.py
@@ -0,0 +1,155 @@
+import copy
+import obspy
+import numpy as np
+import scipy as sp
+import networkx as nx
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+def circumcenter(P: np.ndarray) -> np.ndarray:
+  A, B, C = P
+  D = 2 * (A[0] * (B[1] - C[1]) + B[0] * (C[1] - A[1]) + C[0] * (A[1] - B[1]))
+  Ux = ((A[0]**2 + A[1]**2) * (B[1] - C[1]) + (B[0]**2 + B[1]**2) * (C[1] - A[1]) + (C[0]**2 + C[1]**2) * (A[1] - B[1])) / D
+  Uy = ((A[0]**2 + A[1]**2) * (C[0] - B[0]) + (B[0]**2 + B[1]**2) * (A[0] - C[0]) + (C[0]**2 + C[1]**2) * (B[0] - A[0])) / D
+  return np.array([Ux, Uy])
+
+def delaunay_circumcenter(P : np.ndarray) -> np.ndarray:
+  TRI = sp.spatial.Delaunay(P)
+  return np.asarray([circumcenter(P[simplex]) for simplex in TRI.simplices])
+
+def incenter(P : np.ndarray) -> np.ndarray:
+  A, B, C = P
+  a = np.linalg.norm(B - C)
+  b = np.linalg.norm(C - A)
+  c = np.linalg.norm(A - B)
+  return (a * A + b * B + c * C) / (a + b + c)
+
+def delaunay_incenter(P : np.ndarray) -> np.ndarray:
+  TRI = sp.spatial.Delaunay(P)
+  return np.asarray([incenter(P[simplex]) for simplex in TRI.simplices])
+
+def orthocenter(P : np.ndarray) -> np.ndarray:
+  A, B, C = P
+  a = np.linalg.norm(B - C)
+  b = np.linalg.norm(C - A)
+  c = np.linalg.norm(A - B)
+  return (a**2 * (B + C) + b**2 * (A + C) + c**2 * (A + B)) / (a**2 + b**2 + c**2) - A - B - C
+
+def delaunay_orthocenter(P : np.ndarray) -> np.ndarray:
+  TRI = sp.spatial.Delaunay(P)
+  return np.asarray([orthocenter(P[simplex]) for simplex in TRI.simplices])
+
+def eulercenter(P : np.ndarray) -> np.ndarray:
+  cx, cy = circumcenter(P)
+  ox, oy = orthocenter(P)
+  return (cx + ox) / 2, (cy + oy) / 2
+
+def delaunay_eulercenter(P : np.ndarray) -> np.ndarray:
+  TRI = sp.spatial.Delaunay(P)
+  return np.asarray([eulercenter(P[simplex]) for simplex in TRI.simplices])
+
+def centroid(P : np.ndarray) -> np.ndarray:
+  return P.mean(axis=0)
+
+def delaunay_centroid(P : np.ndarray) -> np.ndarray:
+  TRI = sp.spatial.Delaunay(P)
+  return np.asarray([centroid(P[simplex]) for simplex in TRI.simplices])
+
+class StationHierarchy:
+  def __init__(self, positions, depth = 0):
+    self.t = None
+    self.x = None
+    self.y = None
+    self.z = None
+    self.depth = depth
+    self.graph = nx.Graph()
+    self.positions = positions
+    for i, pos in enumerate(positions): self.graph.add_node(i, pos=pos)
+    if len(positions) > 2:
+      TESS = sp.spatial.Delaunay(positions)
+      CENTROID = nx.Graph()
+      for i, spx in enumerate(TESS.simplices):
+        nx.add_cycle(self.graph, spx)
+        CENTROID.add_node(i, pos=centroid(positions[spx]))
+      C_NODES = copy.deepcopy(CENTROID.nodes())
+      for node in C_NODES:
+        for neighbor in TESS.neighbors[node]:
+          if neighbor > 0: CENTROID.add_edge(node, neighbor)
+      C_COLORS = nx.coloring.equitable_color(CENTROID, num_colors=4)
+      colors = set(C_COLORS.values())
+      self.t = StationHierarchy(
+                 np.asarray([CENTROID.nodes[node]['pos'] for node in C_NODES
+                             if C_COLORS[node] == 0]))
+      if 1 in colors:
+        self.x = StationHierarchy(
+                   np.asarray([CENTROID.nodes[node]['pos'] for node in C_NODES
+                               if C_COLORS[node] == 1]))
+      if 2 in colors:
+       self.y = StationHierarchy(
+                   np.asarray([CENTROID.nodes[node]['pos'] for node in C_NODES
+                               if C_COLORS[node] == 2]))
+      if 3 in colors:
+        self.z = StationHierarchy(
+                   np.asarray([CENTROID.nodes[node]['pos'] for node in C_NODES
+                               if C_COLORS[node] == 3]))
+    elif len(positions) == 2: self.graph.add_edge(0, 1)
+
+  def draw(self, c = 'k', a = 1):
+    nx.draw(self.graph, nx.get_node_attributes(self.graph, 'pos'),
+            node_size=50, node_color=c, alpha=a)
+    frac = 2 / 3
+    if self.t: self.t.draw('r', frac * a)
+    if self.x: self.x.draw('g', frac * a)
+    if self.y: self.y.draw('b', frac * a)
+    if self.z: self.z.draw('m', frac * a)
+
+  def __str__(self, offset = ""):
+    if self.t: return self.t.__str__(offset + "\t")
+    if self.x: return self.x.__str__(offset + "\t")
+    if self.y: return self.y.__str__(offset + "\t")
+    if self.z: return self.z.__str__(offset + "\t")
+    return offset + str(self.positions)
+
+  def tail(self):
+    t = True
+    if self.t:
+      t = False
+      self.t.tail()
+    if self.x:
+      t = False
+      self.x.tail()
+    if self.y:
+      t = False
+      self.y.tail()
+    if self.z:
+      t = False
+      self.z.tail()
+    if t: print(self.positions)
+    return
+
+def station_graph(inventory : obspy.Inventory) -> None:
+  x = [station.longitude for network in inventory for station in network]
+  y = [station.latitude for network in inventory for station in network]
+  station = [station.code for network in inventory for station in network]
+  P = np.asarray(list({(x, y) for x, y in np.c_[x, y].tolist()}))
+  SH = StationHierarchy(P)
+  SH.draw()
+  SH.tail()
+  print(SH)
+  plt.show()
+  return
+
+def main():
+  DATA_DIR = Path(__file__).parent.parent
+  INVENTORY = obspy.Inventory()
+  DATA_PATH = Path(DATA_DIR, "data", "station")
+  for station in DATA_PATH.iterdir():
+    try:
+      S = obspy.read_inventory(station)
+    except: continue
+    INVENTORY.extend(S)
+  station_graph(INVENTORY)
+  return
+
+
+if __name__ == "__main__": main()
